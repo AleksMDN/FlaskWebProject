5c3ec405f35f65ff5ea42e4d7b1edc4ac02704f3
Aleks Do <aleks-do@outlook.com>
2020-09-15 15:39:45 +0300

    Изменен index.html  и  layout

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -13,7 +13,7 @@ def home():
     return render_template(
         'index.html',
         title='Home Page',
-        year=datetime.now().year,
+        year=datetime.now().year
     )
 
 @app.route('/contact')
